662eef71c79f1033bba94c86213c741e433688d2
arthur801031 <arthur801031@gmail.com>
2025-03-09 19:12:10 -0700

    Change default realsense stream to USB 2.0

--- opencv_viewer_example.py
+++ opencv_viewer_example.py
@@ -33,8 +33,8 @@ config.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, 30)
 if device_product_line == 'L500':
     config.enable_stream(rs.stream.color, 960, 540, rs.format.bgr8, 30)
 else:
-    # config.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 30)   # USB 2.0
-    config.enable_stream(rs.stream.color, 1280, 720, rs.format.bgr8, 30)    # USB 3.0
+    config.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 30)   # USB 2.0
+    # config.enable_stream(rs.stream.color, 1280, 720, rs.format.bgr8, 30)    # USB 3.0
 
 # Start streaming
 pipeline.start(config)
